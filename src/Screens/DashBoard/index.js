0ce89d0aa541ce6e6c67e6d3932839d0fcb64363
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2019-08-04 17:37:26 +0700

    Vector icon

--- src/Screens/DashBoard/index.js
+++ src/Screens/DashBoard/index.js
@@ -1,15 +1,19 @@
-/* @flow weak */
-
 import React, {Fragment} from 'react';
 import {
     View,
     Text,
     StyleSheet,
 } from 'react-native';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 const DashBoard = () => (
     <Fragment>
         <Text> MyComponent</Text>
+        <FontAwesome5
+            style={{ color: 'red' }}
+            name="search"
+            size={13}
+        />
     </Fragment>
 );
 
