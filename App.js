8855f3acd47c68e9f2970623315d7fa79f5c7a38
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2019-08-04 16:52:42 +0700

    First commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { SafeAreaView, ScrollView, Text } from 'react-native';
+import DashBoard from './src/Containers/DashBoard/index';
+
+export default class App extends Component {
+    render() {
+        return (
+
+            <SafeAreaView style={{ flex: 1, backgroundColor: '#F88F40' }}>
+                <DashBoard />
+            </SafeAreaView>
+
+        );
+    }
+}
